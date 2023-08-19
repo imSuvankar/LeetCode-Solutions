507487893cf6269d0f89ea2c0c493daa85be9225
Suvankar Das <suvankar_das@outlook.com>
2023-08-19 19:56:47 +0530

    Time: 10 ms (80.85%), Space: 11.3 MB (13.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,9 +3,9 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int, int> intMap;
 
-        for(int i = 0; nums.size(); i++) {
+        for(int i = 0; i < nums.size(); i++) {
             auto it = intMap.find(target - nums[i]);
-            if (it != intMap.end()) return {it->second, i};
+            if(it != intMap.end()) return {it->second, i};
             intMap[nums[i]] = i;
         }
 
