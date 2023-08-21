f09d14cb79b898279f05251bfceadfbcec5eb32e
Suvankar Das <suvankar_das@outlook.com>
2023-08-21 19:53:52 +0530

    Delete 2733-neither-minimum-nor-maximum directory

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int findNonMinOrMax(vector<int>& nums) {
-        if(nums.size() < 3) return -1;
-        
-        sort(nums.begin(), nums.end());
-        return nums[1];
-    }
-};
