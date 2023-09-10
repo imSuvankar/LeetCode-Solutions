81b87ebffb9fcffe547b5f5a1d8d3305eac5bfb2
Suvankar Das <suvankar_das@outlook.com>
2023-09-10 21:17:22 +0530

    Time: 2 ms (44.21%), Space: 8.5 MB (13.01%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        // sorting -> O(nlogn)
+        // counting -> O(n) 
+        // although it's meant to be solved by sorting, and not map-count method
+
+        map<int, int> intMap;
+        for(int i : nums) intMap[i]++;
+
+        vector<int> ans;
+        for(auto i : intMap) ans.insert(ans.end(), i.second, i.first); 
+        
+        nums = ans;
+    }
+};
