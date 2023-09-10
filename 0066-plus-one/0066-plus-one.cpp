7f4e36f47c061e7f459758deaa44c574eaa759d9
Suvankar Das <suvankar_das@outlook.com>
2023-09-10 14:56:12 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (81.63%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& arr) {
+        
+        int i = arr.size() - 1;
+        int newNum = arr[i] + 1;
+        i--;
+
+        vector<int> ans (1, newNum % 10);
+        int carry = 0;
+        if(newNum - 9 > 0) carry = newNum - 9;
+
+        while (i >= 0) {
+            newNum = arr[i] + carry;
+            ans.insert(ans.begin(), newNum % 10);
+            (newNum - 9 > 0) ? carry = newNum - 9 : carry = 0; 
+            i--;
+        }
+
+        if(carry > 0) ans.insert(ans.begin(), carry);
+        return ans;
+    }
+};
