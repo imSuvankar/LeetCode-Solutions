46e0b0d7307d6b4cd60b5fa0f20b0deee41bf8f6
Suvankar Das <suvankar_das@outlook.com>
2023-09-21 00:19:09 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (7.24%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+        /*
+        // APPROACH 1:
+        int n = matrix.size();
+
+        // step 1: transpose
+        for(int i = 0; i < n; i++) {
+            for(int j = i ; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        // step 2: row-wise swapping
+        for(int i = 0; i < n; i++) {
+            int start = 0;
+            int end = n-1;
+            while(start <= end) {
+                swap(matrix[i][start], matrix[i][end]);
+                start++;
+                end--;
+            }
+        }
+        */
+        
+        
+        // APPROACH 2:
+        int n = matrix.size();
+        vector<vector<int>> ans;
+
+        for(int i = 0; i < n; i++) {
+            vector<int> temp;
+            for(int j = n-1; j >= 0; j--) temp.push_back(matrix[j][i]);
+            ans.push_back(temp);
+        }
+
+        matrix = ans;
+    }
+};
