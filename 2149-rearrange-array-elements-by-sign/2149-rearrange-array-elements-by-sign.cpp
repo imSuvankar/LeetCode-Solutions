4946213d1e9ad8c21f9190b39aa57957942c904a
Suvankar Das <suvankar_das@outlook.com>
2023-09-23 13:54:36 +0530

    Time: 153 ms (79.01%), Space: 135 MB (18.04%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         
+        // APPROACH 1:
         vector<int> pos, neg;
         for(int i : nums) (i > 0) ? pos.push_back(i) : neg.push_back(i);
         
-
         // vector<int> ans; 
         // for(int i = 0; i < pos.size(); i++) {
         //     ans.push_back(pos[i]);
@@ -13,12 +13,33 @@ public:
         // }
         // return ans;
 
-
         // same logic, just modifying the nums array, 
         // instead of taking extra space for "ans"
         for(int i = 0; i < nums.size(); i++) {
             nums[i] = (i % 2 == 0) ? pos[i/2] : neg[i/2];
         }
         return nums;
+        
+
+
+        /*
+        // APPROACH 2: two pointer (same complexity)
+        int evenIdx = 0;
+        int oddIdx = 1;
+        vector<int> ans(nums.size(), -1);
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(0 < nums[i]) {
+                ans[evenIdx] = nums[i];
+                evenIdx += 2;
+            }
+            else {
+                ans[oddIdx] = nums[i];
+                oddIdx += 2;
+            }
+        }
+
+        return ans;
+        */
     }
 };
