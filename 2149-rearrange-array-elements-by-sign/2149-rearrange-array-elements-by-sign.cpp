70b36aba7cc25836f817e38653ba8c10bb57f7d8
Suvankar Das <suvankar_das@outlook.com>
2023-09-23 13:36:21 +0530

    Time: 148 ms (88.51%), Space: 134.9 MB (18.87%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -5,12 +5,20 @@ public:
         vector<int> pos, neg;
         for(int i : nums) (i > 0) ? pos.push_back(i) : neg.push_back(i);
         
-        vector<int> ans; 
-        for(int i = 0; i < pos.size(); i++) {
-            ans.push_back(pos[i]);
-            ans.push_back(neg[i]);
-        }
 
-        return ans;
+        // vector<int> ans; 
+        // for(int i = 0; i < pos.size(); i++) {
+        //     ans.push_back(pos[i]);
+        //     ans.push_back(neg[i]);
+        // }
+        // return ans;
+
+
+        // same logic, just modifying the nums array, 
+        // instead of taking extra space for "ans"
+        for(int i = 0; i < nums.size(); i++) {
+            nums[i] = (i % 2 == 0) ? pos[i/2] : neg[i/2];
+        }
+        return nums;
     }
 };
