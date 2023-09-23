923cb40d8c22344605a1e8b3dc1c8c5110b84d56
Suvankar Das <suvankar_das@outlook.com>
2023-09-23 13:55:49 +0530

    Time: 148 ms (88.51%), Space: 135 MB (18.04%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -13,7 +13,7 @@ public:
         // }
         // return ans;
 
-        // same logic, just modifying the nums array, 
+        // same logic, just modifying the nums array,
         // instead of taking extra space for "ans"
         for(int i = 0; i < nums.size(); i++) {
             nums[i] = (i % 2 == 0) ? pos[i/2] : neg[i/2];
