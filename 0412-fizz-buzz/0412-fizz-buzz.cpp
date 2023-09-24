f3618494c49b0922aa9f75cd7394b5bf9598928c
Suvankar Das <suvankar_das@outlook.com>
2023-09-24 21:37:20 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (76.36%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -7,7 +7,7 @@ public:
         for(int i = 1; i <= n; i++) {
             if(i % 3 == 0 && i % 5 != 0) ans.push_back("Fizz");
             else if(i % 3 != 0 && i % 5 == 0) ans.push_back("Buzz");
-            else if(i % 3 == 0 && i % 5 == 0) ans.push_back("FizzBuzz"); 
+            else if(i % 3 == 0 && i % 5 == 0) ans.push_back("FizzBuzz");
             else ans.push_back(to_string(i));
         }
 
