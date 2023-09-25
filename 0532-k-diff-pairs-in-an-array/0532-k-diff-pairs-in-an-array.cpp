14beebab3a2585c7219cd16c05908f2608628dca
Suvankar Das <suvankar_das@outlook.com>
2023-09-25 22:32:13 +0530

    Time: 19 ms (37.06%), Space: 13.6 MB (26.50%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
 
+        /*
+        // APPROACH 1: BRUTE FORCE
         set<pair<int, int>> ans;
         
         for(int i = 0; i < nums.size(); i++) {
@@ -10,6 +12,29 @@ public:
                     ans.insert({min(nums[i], nums[j]), max(nums[i], nums[j])});
                 }
             }
+        }
+
+        return ans.size();
+        */
+
+
+
+        // APPROACH 2: TWO POINTER
+        sort(nums.begin(), nums.end());
+
+        int i = 0; 
+        int j = 1;
+        set<pair<int, int>> ans;
+
+        while(j < nums.size()) {
+            if(nums[j] - nums[i] == k) {
+                ans.insert({nums[i], nums[j]});
+                i++, j++;
+            }
+            else if(nums[j] - nums[i] < k) j++;
+            else i++;
+
+            if(i == j) j++;
         }
 
         return ans.size();
