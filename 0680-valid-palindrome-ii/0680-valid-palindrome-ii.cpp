74ccd1396649194d28a5b03674ddf05e2822103b
Suvankar Das <suvankar_das@outlook.com>
2023-09-29 22:11:36 +0530

    Time: 66 ms (6.96%), Space: 24.2 MB (29.52%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    bool check(string s, int i , int j) {
+        while(i < j) {
+            if(s[i] != s[j]) return false;
+            i++, j--;
+        }
+        return true;
+    }
+
+    
+public:
+    bool validPalindrome(string s) {
+        int i = 0;
+        int j = s.size()-1;
+
+        while(i < j) {
+            if(s[i] != s[j]) return check(s, i, j-1) || check(s, i+1, j);
+            i++, j--;
+        }
+
+        return true;
+    }
+};
