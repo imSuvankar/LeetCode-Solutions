797316ced87def72fdadec793a23f4b1191bf560
Suvankar Das <suvankar_das@outlook.com>
2023-09-29 20:41:02 +0530

    Time: 3 ms (71.51%), Space: 7 MB (19.19%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
+        
+        /*
+        // RECURSIVE
         if(s.find(part) == string::npos) return s;
 
         auto it = s.find(part); 
         s.erase(it, part.size());
         return removeOccurrences(s, part);
+        */
+
+        
+        // ITERATIVE
+        while(s.find(part) != string::npos) s.erase(s.find(part), part.size());
+        return s;
     }
 };
