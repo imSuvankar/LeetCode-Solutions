12623e236afb2c38728747ae9169fab6283c9f96
Suvankar Das <suvankar_das@outlook.com>
2023-09-30 20:00:41 +0530

    Time: 9 ms (52.41%), Space: 13.6 MB (28.60%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,3 +1,5 @@
+/*
+// APPROACH 1: O(n^3) -> checking for every substring
 class Solution {
 private:
     bool check(string s) {
@@ -6,20 +8,35 @@ private:
         return true;
     }
 
-
 public:
     int countSubstrings(string s) {
         int ans = 0;
-
         for(int i = 0; i < s.size(); i++) {
             string subString = "";
-
             for(int j = i; j < s.size(); j++) {
                 subString += s[j];
                 if(check(subString)) ans++;
             }
         }
+        return ans;
+    }
+};
+*/
 
+
+// APPROACH 2: O(n) -> even-odd apprach (LB)
+class Solution {
+private:
+    int expand(string s, int i, int j) {
+        int c = 0;
+        while(0 <= i && j < s.size() && s[i--] == s[j++]) c++;
+        return c;
+    }
+
+public:
+    int countSubstrings(string s) {
+        int ans = 0;
+        for(int i = 0; i < s.size(); i++) ans += expand(s, i, i) + expand(s, i, i+1);
         return ans;
     }
 };
