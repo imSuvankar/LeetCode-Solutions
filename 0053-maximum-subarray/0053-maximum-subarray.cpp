9f7aac72b98b4932d3114d5e4b29beb3c65f1e2d
Suvankar Das <suvankar_das@outlook.com>
2023-09-30 22:05:23 +0530

    Time: 68 ms (96.98%), Space: 68 MB (25.34%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        
-        // kadanes algorithm
+
+        // kadanes algorithm 
         int ans = INT_MIN;
         int currMax = 0;
         
