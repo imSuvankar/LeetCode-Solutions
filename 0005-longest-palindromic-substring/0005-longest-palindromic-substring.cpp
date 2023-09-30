0fe1834c48b881e34738471840f72d190bb00c2f
Suvankar Das <suvankar_das@outlook.com>
2023-09-30 20:45:09 +0530

    Time: 51 ms (58.09%), Space: 171.9 MB (19.53%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,25 @@
+// same as -> 647. Palindromic Substrings
+
+class Solution {
+private:
+    string expand(string s, int i, int j) {
+        while(0 <= i && j < s.size() && s[i] == s[j]) i--, j++;
+        return s.substr(i+1, j-i-1);
+    }
+
+
+public:
+    string longestPalindrome(string s) {
+        string ans = "";
+
+        for(int i = 0; i < s.size(); i++) {
+            string odd = expand(s, i, i);
+            string even = expand(s, i, i+1);
+
+            if(ans.size() < odd.size()) ans = odd;
+            if(ans.size() < even.size()) ans = even;
+        }
+
+        return ans;
+    }
+};
