eae667a0b1194283ae7eef645e235113786a455f
Suvankar Das <suvankar_das@outlook.com>
2023-10-03 03:13:05 +0530

    Time: 66 ms (12.34%), Space: 29.7 MB (7.50%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    map<char, int> getMapping(string s) {
+        map<char, int> ans;
+        for(char c : s) ans[c]++;
+        return ans;
+    }
+
+
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        if(strs.size() == 1 && strs[0].size() == 0) return {{""}};
+
+
+        vector<pair<map<char, int>, string>> mapping;
+        for(string s : strs) mapping.push_back({getMapping(s), s});
+        sort(mapping.begin(), mapping.end());
+
+        int idx = 0;
+        auto currMapping = mapping[0].first;
+        vector<vector<string>> ans(1, {{mapping[0].second}});
+
+        for(int i = 1; i < strs.size(); i++) {
+            if(mapping[i].first == currMapping) ans[idx].push_back(mapping[i].second);
+            else {
+                ans.push_back({{mapping[i].second}});
+                currMapping = mapping[i].first;
+                idx++;
+            }
+        }
+
+        return ans;
+    }
+};
