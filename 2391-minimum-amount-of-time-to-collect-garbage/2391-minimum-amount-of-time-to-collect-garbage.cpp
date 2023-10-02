a36d006f74b77242b0ebefa4044c36b92dc5e7de
Suvankar Das <suvankar_das@outlook.com>
2023-10-02 13:36:32 +0530

    Time: 182 ms (33.11%), Space: 107.2 MB (9.21%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -5,46 +5,46 @@ public:
         int n = garbage.size();
 
         // STEP 1:
-        vector<int> metal(n, 0);
-        vector<int> paper(n, 0);
-        vector<int> glass(n, 0);
+        vector<int> metalInHome(n, 0);
+        vector<int> paperInHome(n, 0);
+        vector<int> glassInHome(n, 0);
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < garbage[i].size(); j++) {
-                if(garbage[i][j] == 'M') metal[i]++;
-                if(garbage[i][j] == 'P') paper[i]++;
-                if(garbage[i][j] == 'G') glass[i]++;
+                if(garbage[i][j] == 'M') metalInHome[i]++;
+                else if(garbage[i][j] == 'P') paperInHome[i]++;
+                else if(garbage[i][j] == 'G') glassInHome[i]++;
             }
         }
         
         // STEP 2:
-        int metalCost = metal[0];
-        int paperCost = paper[0];
-        int glassCost = glass[0];
-        int totalMinsTill = 0;
-        int metalMins = 0;
-        int paperMins = 0;
-        int glassMins = 0;
+        int metalCollect = metalInHome[0];
+        int metalTravels = 0;
+        int paperCollect = paperInHome[0];
+        int paperTravels = 0;
+        int glassCollect = glassInHome[0];
+        int glassTravels = 0;
+        int totalMinTill = 0;
 
         for(int i = 1; i < n; i++) {
-            totalMinsTill += travel[i-1];
+            totalMinTill += travel[i-1];
 
-            if(metal[i]) {
-                metalCost += metal[i];
-                metalMins = totalMinsTill;
+            if(metalInHome[i]) {
+                metalCollect += metalInHome[i];
+                metalTravels = totalMinTill;
             }
 
-            if(paper[i]) {
-                paperCost += paper[i];
-                paperMins = totalMinsTill;
+            if(paperInHome[i]) {
+                paperCollect += paperInHome[i];
+                paperTravels = totalMinTill;
             }
 
-            if(glass[i]) {
-                glassCost += glass[i];
-                glassMins = totalMinsTill;
+            if(glassInHome[i]) {
+                glassCollect += glassInHome[i];
+                glassTravels = totalMinTill;
             }
         }
 
-        return metalCost + metalMins + paperCost + paperMins + glassCost + glassMins;
+        return metalCollect + metalTravels + paperCollect + paperTravels + glassCollect + glassTravels;
     }
 };
