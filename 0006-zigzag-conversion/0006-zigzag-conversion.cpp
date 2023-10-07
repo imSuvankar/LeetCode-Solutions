851d7bcd6c51264aadad23b67f8fceab10506a92
Suvankar Das <suvankar_das@outlook.com>
2023-10-08 02:41:15 +0530

    Time: 7 ms (81.61%), Space: 11 MB (22.59%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        
+        vector<string> rows(numRows, "");
+
+        int c = 0;
+        while(c < s.size()) {
+
+            for(int i = 0; i < numRows && c < s.size(); i++) {
+                rows[i] += s[c];
+                c++;
+            }
+
+            for(int i = numRows - 2; i > 0 && c < s.size(); i--) {
+                rows[i] += s[c];
+                c++;
+            }
+        }
+
+        string ans = "";
+        for(string st : rows) ans += st;
+        return ans;
+    }
+};
