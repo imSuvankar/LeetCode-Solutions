01d89e14affe24dbb1591ee7cb29241306894b16
Suvankar Das <suvankar_das@outlook.com>
2023-10-12 15:07:56 +0530

    Time: 3 ms (94.14%), Space: 13 MB (29.76%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -2,6 +2,6 @@ class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
         nums.insert(nums.end(), nums.begin(), nums.end());
-        return nums;
+        return nums; 
     }
 };
